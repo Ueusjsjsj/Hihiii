daa729b51510bd11169d63f9525277ba2dd29ab5
Namprox <namky1602@gmail.com>
2023-04-26 13:18:12 +0700

    init project

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bạn ơi!',
+    introDesc: `Trái đất vốn lạ thường,
+    mà sao bạn cứ đi nhầm đường,
+    lạc vào tim mình lẻ loi,
+    đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích bạn <33',
+    btnNo: 'Không, bạn thân à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà🥰. Yêu bạn nhiều nhiều😘😘',
+    messDesc: 'Ngày mai chúng mình hẹn hò nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
